6aa94d1ecedd9087a82f2e31a23127a09723bff1
Michal Ziemski <michal@terrestryal.com>
2021-12-26 11:42:26 +0100

    FIX: Exit with error on empty DataFrame

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 import pandas as pd
 from requests import get
 
@@ -18,7 +20,11 @@ def run():
     stations_df = pd.DataFrame(stations_info)
     stations_df.sort_values('id', inplace=True)
     stations_df.set_index('id', drop=True, inplace=True)
-    stations_df.to_csv('./stations/stations.csv')
+
+    if len(stations_df) > 0:
+        stations_df.to_csv('./stations/stations.csv')
+    else:
+        sys.exit(1)
 
 
 if __name__ == "__main__":
